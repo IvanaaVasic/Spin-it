c77d131975bb957b84770420dae0df91d9109350
IvanaV <ivana.p.vasic@gmail.com>
2020-05-25 10:59:31 +0200

    bottom to top button

--- js/common.js
+++ js/common.js
@@ -111,3 +111,25 @@ rightBtn.addEventListener("mouseleave", stopMovement);
     galleryItems[i].addEventListener("click", selectItem);
   }
 })();
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}

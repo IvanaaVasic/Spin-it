f27192d97349c64fbe22be9b6b99eaddf49ca034
IvanaV <ivana.p.vasic@gmail.com>
2020-06-02 01:07:04 +0200

    zoom single page lance

--- js/products.js
+++ js/products.js
@@ -1,23 +1,104 @@
-var count = 1;
+var itemHolderCount = 1;
 
-function myFunction(imgs) {
+function changeMainPicture(imgs) {
+  console.log(imgs);
   var expandImg = document.getElementById("expandedImg");
   expandImg.src = imgs.src;
   expandImg.parentElement.style.display = "block";
 }
 
 function addPieces() {
-  console.log(count);
-
-  count++;
+  if (itemHolderCount >= 20) {
+    return;
+  }
+  itemHolderCount++;
   var numberHolder = document.querySelector(".count-holder");
-  var plus = document.querySelector(".plus-button");
-  numberHolder.innerHTML = count;
+  numberHolder.innerHTML = itemHolderCount;
 }
 function subPieces() {
-  console.log(count);
-  count--;
+  itemHolderCount--;
   var numberHolder = document.querySelector(".count-holder");
-  var minus = document.querySelector(".minus-button");
-  numberHolder.innerHTML = count;
+  numberHolder.innerHTML = itemHolderCount;
 }
+
+function imageZoom(img, resultID) {
+  document.getElementById("myresult").style.display = "block";
+
+  var img, lens, result, cx, cy;
+  result = document.getElementById(resultID);
+
+  lens = document.createElement("DIV");
+  lens.setAttribute("class", "img-zoom-lens");
+
+  img.parentElement.insertBefore(lens, img);
+
+  cx = result.offsetWidth / lens.offsetWidth;
+  cy = result.offsetHeight / lens.offsetHeight;
+
+  result.style.backgroundImage = "url('" + img.src + "')";
+  result.style.backgroundSize = img.width * cx + "px " + img.height * cy + "px";
+
+  lens.addEventListener("mousemove", moveLens);
+  img.addEventListener("mousemove", moveLens);
+
+  lens.addEventListener("mouseleave", function () {
+    document.getElementById("myresult").style.display = "none";
+
+    let lenses = document.getElementsByClassName("img-zoom-lens");
+    while (lenses[0]) {
+      lenses[0].parentNode.removeChild(lenses[0]);
+    }
+  });
+
+  lens.addEventListener("mouseenter", function () {
+    document.getElementById("myresult").style.display = "block";
+  });
+
+  function moveLens(e) {
+    var pos, x, y;
+
+    e.preventDefault();
+
+    pos = getCursorPos(e);
+
+    x = pos.x - lens.offsetWidth / 2;
+    y = pos.y - lens.offsetHeight / 2;
+
+    if (x > img.width - lens.offsetWidth) {
+      x = img.width - lens.offsetWidth;
+    }
+    if (x < 0) {
+      x = 0;
+    }
+    if (y > img.height - lens.offsetHeight) {
+      y = img.height - lens.offsetHeight;
+    }
+    if (y < 0) {
+      y = 0;
+    }
+
+    lens.style.left = x + "px";
+    lens.style.top = y + "px";
+
+    result.style.backgroundPosition = "-" + x * cx + "px -" + y * cy + "px";
+  }
+  function getCursorPos(e) {
+    var a,
+      x = 0,
+      y = 0;
+    e = e || window.event;
+
+    a = img.getBoundingClientRect();
+
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return { x: x, y: y };
+  }
+}
+
+document.getElementById("expandedImg").addEventListener("mouseenter", () => {
+  imageZoom(document.getElementById("expandedImg"), "myresult");
+});

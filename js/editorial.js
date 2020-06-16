b804439ffea68d1eb09d3d28f2419be2dd75b55c
IvanaV <ivana.p.vasic@gmail.com>
2020-06-16 16:32:38 +0200

    some changes

--- js/editorial.js
+++ js/editorial.js
@@ -14,27 +14,31 @@ sideMenu[0].addEventListener("mouseleave", function () {
 sideMenu[1].addEventListener("mouseover", function () {
   imgFirst[0].children[1].setAttribute("id", "zoom");
   sideMenu[1].style.color = "#bf7979";
+  imgFirst[0].children[1].src = "imgEditorial/group-hug-name.png";
 });
 sideMenu[1].addEventListener("mouseleave", function () {
   imgFirst[0].children[1].removeAttribute("id");
   sideMenu[1].style.color = "#636677";
+  imgFirst[0].children[1].src = "imgEditorial/picture-2-ribbon.png";
 });
 
 sideMenu[2].addEventListener("mouseover", function () {
-  imgSecond[0].children[0].setAttribute("id", "zoom");
+  imgSecond[0].children[1].setAttribute("id", "zoom");
   sideMenu[2].style.color = "#bf7979";
 });
 sideMenu[2].addEventListener("mouseleave", function () {
-  imgSecond[0].children[0].removeAttribute("id");
+  imgSecond[0].children[1].removeAttribute("id");
   sideMenu[2].style.color = "#636677";
 });
 sideMenu[3].addEventListener("mouseover", function () {
-  imgSecond[0].children[1].setAttribute("id", "zoom");
+  imgSecond[0].children[0].setAttribute("id", "zoom");
   sideMenu[3].style.color = "#bf7979";
+  imgSecond[0].children[0].src = "imgEditorial/persona1.png";
 });
 sideMenu[3].addEventListener("mouseleave", function () {
-  imgSecond[0].children[1].removeAttribute("id");
+  imgSecond[0].children[0].removeAttribute("id");
   sideMenu[3].style.color = "#636677";
+  imgSecond[0].children[0].src = "imgEditorial/picture-3-ribbon.png";
 });
 
 imgFirst[0].children[0].addEventListener("mouseover", function () {
@@ -48,24 +52,28 @@ imgFirst[0].children[0].addEventListener("mouseleave", function () {
 imgFirst[0].children[1].addEventListener("mouseover", function () {
   sideMenu[1].style.color = "#bf7979";
   imgFirst[0].children[1].setAttribute("id", "zoom");
+  imgFirst[0].children[1].src = "imgEditorial/group-hug-name.png";
 });
 imgFirst[0].children[1].addEventListener("mouseleave", function () {
   sideMenu[1].style.color = "#636677";
   imgFirst[0].children[1].removeAttribute("id");
+  imgFirst[0].children[1].src = "imgEditorial/picture-2-ribbon.png";
 });
 imgSecond[0].children[0].addEventListener("mouseover", function () {
-  sideMenu[2].style.color = "#bf7979";
+  sideMenu[3].style.color = "#bf7979";
   imgSecond[0].children[0].setAttribute("id", "zoom");
+  imgSecond[0].children[0].src = "imgEditorial/persona1.png";
 });
 imgSecond[0].children[0].addEventListener("mouseleave", function () {
-  sideMenu[2].style.color = "#636677";
+  sideMenu[3].style.color = "#636677";
   imgSecond[0].children[0].removeAttribute("id");
+  imgSecond[0].children[0].src = "imgEditorial/picture-3-ribbon.png";
 });
 imgSecond[0].children[1].addEventListener("mouseover", function () {
-  sideMenu[3].style.color = "#bf7979";
+  sideMenu[2].style.color = "#bf7979";
   imgSecond[0].children[1].setAttribute("id", "zoom");
 });
 imgSecond[0].children[1].addEventListener("mouseleave", function () {
-  sideMenu[3].style.color = "#636677";
+  sideMenu[2].style.color = "#636677";
   imgSecond[0].children[1].removeAttribute("id");
 });

62bb75532e8a6d14f9f5529c91dc8bf12d5f1a90
IvanaV <ivana.p.vasic@gmail.com>
2020-06-26 12:00:50 +0200

    popup

--- js/editorial.js
+++ js/editorial.js
@@ -5,16 +5,18 @@ var imgSecond = document.getElementsByClassName("second-row-pictures");
 sideMenu[0].addEventListener("mouseover", function () {
   imgFirst[0].children[0].setAttribute("id", "zoom");
   sideMenu[0].style.color = "#bf7979";
+  imgFirst[0].children[0].src = "imgEditorial/Artboard2.png";
 });
 sideMenu[0].addEventListener("mouseleave", function () {
   imgFirst[0].children[0].removeAttribute("id");
   sideMenu[0].style.color = "#636677";
+  imgFirst[0].children[0].src = "imgEditorial/picture-1-ribbon.png";
 });
 
 sideMenu[1].addEventListener("mouseover", function () {
   imgFirst[0].children[1].setAttribute("id", "zoom");
   sideMenu[1].style.color = "#bf7979";
-  imgFirst[0].children[1].src = "imgEditorial/group-hug-name.png";
+  imgFirst[0].children[1].src = "imgEditorial/Artboard1.png";
 });
 sideMenu[1].addEventListener("mouseleave", function () {
   imgFirst[0].children[1].removeAttribute("id");
@@ -25,15 +27,17 @@ sideMenu[1].addEventListener("mouseleave", function () {
 sideMenu[2].addEventListener("mouseover", function () {
   imgSecond[0].children[1].setAttribute("id", "zoom");
   sideMenu[2].style.color = "#bf7979";
+  imgSecond[0].children[1].src = "imgEditorial/Artboard4.png";
 });
 sideMenu[2].addEventListener("mouseleave", function () {
   imgSecond[0].children[1].removeAttribute("id");
   sideMenu[2].style.color = "#636677";
+  imgSecond[0].children[1].src = "imgEditorial/picture-4-ribbon.png";
 });
 sideMenu[3].addEventListener("mouseover", function () {
   imgSecond[0].children[0].setAttribute("id", "zoom");
   sideMenu[3].style.color = "#bf7979";
-  imgSecond[0].children[0].src = "imgEditorial/persona1.png";
+  imgSecond[0].children[0].src = "imgEditorial/Artboard3.png";
 });
 sideMenu[3].addEventListener("mouseleave", function () {
   imgSecond[0].children[0].removeAttribute("id");
@@ -44,15 +48,17 @@ sideMenu[3].addEventListener("mouseleave", function () {
 imgFirst[0].children[0].addEventListener("mouseover", function () {
   sideMenu[0].style.color = "#bf7979";
   imgFirst[0].children[0].setAttribute("id", "zoom");
+  imgFirst[0].children[0].src = "imgEditorial/Artboard2.png";
 });
 imgFirst[0].children[0].addEventListener("mouseleave", function () {
   sideMenu[0].style.color = "#636677";
   imgFirst[0].children[0].removeAttribute("id");
+  imgFirst[0].children[0].src = "imgEditorial/picture-1-ribbon.png";
 });
 imgFirst[0].children[1].addEventListener("mouseover", function () {
   sideMenu[1].style.color = "#bf7979";
   imgFirst[0].children[1].setAttribute("id", "zoom");
-  imgFirst[0].children[1].src = "imgEditorial/group-hug-name.png";
+  imgFirst[0].children[1].src = "imgEditorial/Artboard1.png";
 });
 imgFirst[0].children[1].addEventListener("mouseleave", function () {
   sideMenu[1].style.color = "#636677";
@@ -62,7 +68,7 @@ imgFirst[0].children[1].addEventListener("mouseleave", function () {
 imgSecond[0].children[0].addEventListener("mouseover", function () {
   sideMenu[3].style.color = "#bf7979";
   imgSecond[0].children[0].setAttribute("id", "zoom");
-  imgSecond[0].children[0].src = "imgEditorial/persona1.png";
+  imgSecond[0].children[0].src = "imgEditorial/Artboard3.png";
 });
 imgSecond[0].children[0].addEventListener("mouseleave", function () {
   sideMenu[3].style.color = "#636677";
@@ -72,8 +78,10 @@ imgSecond[0].children[0].addEventListener("mouseleave", function () {
 imgSecond[0].children[1].addEventListener("mouseover", function () {
   sideMenu[2].style.color = "#bf7979";
   imgSecond[0].children[1].setAttribute("id", "zoom");
+  imgSecond[0].children[1].src = "imgEditorial/Artboard4.png";
 });
 imgSecond[0].children[1].addEventListener("mouseleave", function () {
   sideMenu[2].style.color = "#636677";
   imgSecond[0].children[1].removeAttribute("id");
+  imgSecond[0].children[1].src = "imgEditorial/picture-4-ribbon.png";
 });
---
+++ js/popup.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#thover").click(function () {
+    $(this).fadeOut();
+    $("#tpopup").fadeOut();
+  });
+
+  $("#tclose").click(function () {
+    $("#thover").fadeOut();
+    $("#tpopup").fadeOut();
+  });
+});
